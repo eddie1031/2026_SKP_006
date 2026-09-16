f160555d434e61ad8d82917d1e776877376bf127
eddie1031 <archaco123@gmail.com>
2026-09-16 09:54:46 +0900

    print_content

---
+++ Readme.md
@@ -0,0 +1,5 @@
+```shell
+streamlit run <실행하고자 하는 파이썬 파일(경로)>
+
+streamlit run ./main.py
+```
---
+++ print_contents.py
@@ -0,0 +1,95 @@
+"""
+streamlit에서 텍스트를 사용하는 방법
+
+title()
+subtitle()
+header()
+write()
+text()
+code()
+markdown()
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+MAIN_TITLE = 'Hello, Streamlit'
+
+st.title(MAIN_TITLE)
+st.header('데이터 애플리케이션')
+st.subheader('대시보드 페이지 구성')
+st.write('streamlit은 대시보드를 쉽게 구성할 수 있습니다.')
+
+st.caption('쉽게 만들수 있어요!')
+
+st.markdown('---')
+
+st.markdown("""
+### 매출 분석 결과
+
+이번달 주요 지표입니다.
+
+* 총 주문수: **1000건**
+- 총 매출: 3500000만원
+- 평균 주문 금액: 28000원
+
+매출은 지난달 보다 *8.2%* 증가 했습니다.
+""")
+
+st.markdown('---')
+
+scores = [ 78, 85, 92, 88, 95 ]
+score_avg = sum(scores) / len(scores)
+
+st.subheader('학생 평균')
+st.markdown(f'현재 학생들의 평균점수는 **{score_avg:.2f}** 점입니다.')
+
+st.markdown('---')
+
+example_sql = """
+SELECT
+  category, product_name, unit_price
+FROM
+  orders
+GROUP BY
+  category
+"""
+
+
+st.code(
+    example_sql,
+    language='sql'
+)
+
+st.markdown('---')
+
+sales = pd.DataFrame({
+    '월': [
+        '1월',
+        '2월',
+        '3월',
+        '4월',
+    ],
+    '가격': [
+        1200,
+        1500,
+        1350,
+        1800,
+    ],
+    '판매량': [
+        3,
+        8,
+        15,
+        20
+    ]
+})
+
+sales['매출'] = sales['가격'] * sales['판매량']
+
+st.title('월별 매출')
+
+st.dataframe(
+    sales,
+    hide_index=True,
+)
